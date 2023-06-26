53df0d0ff0edf1911ddf8b212e81f9b1ccf7b0a2
Clara Holm <clara.holm@hotmail.com>
2023-06-26 15:38:11 +0200

    relations and validations for models

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,2 +1,3 @@
 class Chatroom < ApplicationRecord
+  belongs_to :events_users
 end
--- app/models/hike.rb
+++ app/models/hike.rb
@@ -1,2 +1,10 @@
 class Hike < ApplicationRecord
+  belongs_to :event, presence: true
+  validates :distance, presence: true
+  validates :elevation, presence: true
+  validates :duration, presence: true
+  validates :starting_lat, presence: true
+  validates :starting_lng, presence: true
+  validates :ending_lat, presence: true
+  validates :ending_lng, presence: true
 end
