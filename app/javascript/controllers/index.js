52e1641eea45c929541bbb7f1da981e886782ca4
Angie Duhard <angie.duhard@gmail.com>
2023-06-28 13:51:11 +0200

    stimulus pour new event

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,8 @@
 
 import { application } from "./application"
 
+import EventRouteController from "./event_route_controller"
+application.register("event-route", EventRouteController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
