8bbbaea10f1e0173ddfde01adff85bb2c54c4284
Angie Duhard <angie.duhard@gmail.com>
2023-06-28 15:47:14 +0200

    front for home and event

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
+    @events = Event.all
   end
 
   def dashboard
+    # @eventsbooked = Events.join(:booking).where(booking: { user: current_user })
+    @myevents = current_user.events
   end
 end
