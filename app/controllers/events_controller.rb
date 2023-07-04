b13f862db9aed446484dc343fb57e9bf193f1f53
Adrien Le Doussal <adoussal@gmail.com>
2023-07-04 16:56:15 +0200

    chatroom+seed

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -35,6 +35,8 @@ end
     @event.hike = Hike.new
     authorize @event
     if @event.save!
+      # @chatroom = Chatroom.create!(event: @event)
+
       redirect_to events_path
     else
       render :new, status: :unprocessable_entity
--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,3 +1,4 @@
 class Chatroom < ApplicationRecord
   belongs_to :event, optional: true
+  has_many :messages, dependent: :destroy
 end
