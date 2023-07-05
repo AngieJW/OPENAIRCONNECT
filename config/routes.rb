c88f5b12cd97d58751453d10778ba512e3b5d1c2
Tereva Millet <59101444+TerevaM@users.noreply.github.com>
2023-07-05 08:39:42 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -18,10 +18,6 @@ Rails.application.routes.draw do
     resources :items, only: %i[index show new create edit update destroy]
     resources :memories, only: %i[show new create edit update]
   end
-<<<<<<< HEAD
-=======
-
->>>>>>> 97b2eace0d6f16c53fc25ae03d3e70e63115b6f5
   patch 'create_brings', to: "item_brings#create_brings", as: :update_items
   resources :bookings, only: %i[destroy]
 end
