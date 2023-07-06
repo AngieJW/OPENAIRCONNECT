433a92e1c5c9dc154eda49865d5498563ecedc9a
Tereva Millet <terevamillet@yahoo.fr>
2023-07-06 11:32:19 +0200

    debut

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -5,6 +5,6 @@ import "flatpickr/dist/l10n/fr.js";
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
   connect() {
-    flatpickr(this.element, {   disableMobile: "true", dateFormat: 'd-m-Y',  minDate: "today", defaultDate: "today",  locale: "fr"})
+    flatpickr(this.element, {   disableMobile: "true", dateFormat: 'd-m-Y',  minDate: "today",  locale: "fr"})
   }
 }
